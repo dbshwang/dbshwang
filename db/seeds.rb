516d1252734ed317cde320c35ade70fc54ae53ea
Daniel Hwang <daniel.hwang11@gmail.com>
2018-01-20 14:55:21 -0500

    Comment out seeding for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-negroni = Drink.find_or_initialize_by(
-  title: "Sparkling Negroni",
-  description: "The perfect cocktail for sipping after an alfresco dinner on a summer night, Negronis get their red hue and herbaceous beginning from the Italian apéritif Campari, which is mellowed out by floral gin and sweet vermouth. Top off your drink with some bubbly, and enjoy.",
-  steps: "Combine the first three ingredients in an ice-filled cocktail shaker. Shake until cold, then strain the mixture into a glass. Top with prosecco, and garnish with the orange twist.",
-  source: "http://www.architecturaldigest.com/gallery/4-easy-entertaining-summer-cocktail-recipes-5-ingredients-or-less",
-).save!
-negroni.ingredients.find_or_initialize_by(description: "⅓ oz. Campari").save!
-negroni.ingredients.find_or_initialize_by(description: "⅓ oz. gin").save!
-negroni.ingredients.find_or_initialize_by(description: "⅓ oz. sweet vermouth").save!
-negroni.ingredients.find_or_initialize_by(description: "Chilled prosecco, or other sparkling wine, for topping").save!
-negroni.ingredients.find_or_initialize_by(description: "Orange peel twist (optional)").save!
+# negroni = Drink.find_or_initialize_by(
+#   title: "Sparkling Negroni",
+#   description: "The perfect cocktail for sipping after an alfresco dinner on a summer night, Negronis get their red hue and herbaceous beginning from the Italian apéritif Campari, which is mellowed out by floral gin and sweet vermouth. Top off your drink with some bubbly, and enjoy.",
+#   steps: "Combine the first three ingredients in an ice-filled cocktail shaker. Shake until cold, then strain the mixture into a glass. Top with prosecco, and garnish with the orange twist.",
+#   source: "http://www.architecturaldigest.com/gallery/4-easy-entertaining-summer-cocktail-recipes-5-ingredients-or-less",
+# ).save!
+# negroni.ingredients.find_or_initialize_by(description: "⅓ oz. Campari").save!
+# negroni.ingredients.find_or_initialize_by(description: "⅓ oz. gin").save!
+# negroni.ingredients.find_or_initialize_by(description: "⅓ oz. sweet vermouth").save!
+# negroni.ingredients.find_or_initialize_by(description: "Chilled prosecco, or other sparkling wine, for topping").save!
+# negroni.ingredients.find_or_initialize_by(description: "Orange peel twist (optional)").save!
 
-margarita = Drink.find_or_initialize_by(
-  title: "Pineapple-Jalapeño Margarita",
-  description: "No margarita is complete without fresh-squeezed lime juice—there’s something about the sour punch of citrus that goes so well with the smokiness of tequila. To stir things up, try adding pineapple juice to the mix and muddling in some jalapeño peppers for a little heat.",
-  steps: "Pour the lime juice and jalapeños into a shaker and muddle with the back of a wood spoon. Fill with ice. Pour in tequila, pineapple juice, and Grand Marnier. Shake until chilled. Dip the rim of a rocks glass in water, then dip it in coarse salt. Fill the glass with ice, and strain the cocktail into the glass. Garnish with pineapple wedge and peel and jalapeño slices.",
-  source: "http://www.architecturaldigest.com/gallery/4-easy-entertaining-summer-cocktail-recipes-5-ingredients-or-less"
-).save!
-margarita.ingredients.find_or_initialize_by(description: "½ oz. fresh lime juice").save!
-margarita.ingredients.find_or_initialize_by(description: "⅓ of a large jalapeño, sliced, plus more for garnish").save!
-margarita.ingredients.find_or_initialize_by(description: "1¾ oz. tequila").save!
-margarita.ingredients.find_or_initialize_by(description: "1½ oz. fresh pineapple juice").save!
-margarita.ingredients.find_or_initialize_by(description: "½ oz. Grand Marnier or other orange liqueur").save!
-margarita.ingredients.find_or_initialize_by(description: "Coarse salt, for rimming glass").save!
-margarita.ingredients.find_or_initialize_by(description: "Pineapple wedge and peel, for garnish").save!
+# margarita = Drink.find_or_initialize_by(
+#   title: "Pineapple-Jalapeño Margarita",
+#   description: "No margarita is complete without fresh-squeezed lime juice—there’s something about the sour punch of citrus that goes so well with the smokiness of tequila. To stir things up, try adding pineapple juice to the mix and muddling in some jalapeño peppers for a little heat.",
+#   steps: "Pour the lime juice and jalapeños into a shaker and muddle with the back of a wood spoon. Fill with ice. Pour in tequila, pineapple juice, and Grand Marnier. Shake until chilled. Dip the rim of a rocks glass in water, then dip it in coarse salt. Fill the glass with ice, and strain the cocktail into the glass. Garnish with pineapple wedge and peel and jalapeño slices.",
+#   source: "http://www.architecturaldigest.com/gallery/4-easy-entertaining-summer-cocktail-recipes-5-ingredients-or-less"
+# ).save!
+# margarita.ingredients.find_or_initialize_by(description: "½ oz. fresh lime juice").save!
+# margarita.ingredients.find_or_initialize_by(description: "⅓ of a large jalapeño, sliced, plus more for garnish").save!
+# margarita.ingredients.find_or_initialize_by(description: "1¾ oz. tequila").save!
+# margarita.ingredients.find_or_initialize_by(description: "1½ oz. fresh pineapple juice").save!
+# margarita.ingredients.find_or_initialize_by(description: "½ oz. Grand Marnier or other orange liqueur").save!
+# margarita.ingredients.find_or_initialize_by(description: "Coarse salt, for rimming glass").save!
+# margarita.ingredients.find_or_initialize_by(description: "Pineapple wedge and peel, for garnish").save!
 
-Drink.find_or_initialize_by(
-  title: "Testing deployment migration and seed",
-  description: "Hoping that this will work properly without breaking anything",
-  steps: "Just heroku push that to master baby",
-  source: "http://aspiringwebdev.com/run-rails-migrations-automatically-on-heroku/"
-).save!
+# Drink.find_or_initialize_by(
+#   title: "Testing deployment migration and seed",
+#   description: "Hoping that this will work properly without breaking anything",
+#   steps: "Just heroku push that to master baby",
+#   source: "http://aspiringwebdev.com/run-rails-migrations-automatically-on-heroku/"
+# ).save!
