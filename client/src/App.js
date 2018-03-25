36b5733edbc01b8390ce6081c14a4c4b57f559fc
Daniel Hwang <daniel.hwang11@gmail.com>
2018-03-24 21:16:57 -0400

    Set up basic utilities and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import 'App.css';
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from 'containers/home/HomePage';
--- client/src/containers/home/HomePage.js
+++ client/src/containers/home/HomePage.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from 'logo.svg';
-import 'App.css';
 
 class HomePage extends Component {
   componentDidMount() {
@@ -12,14 +10,9 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1 className='gold'>Daniel Hwang</h1>
+        <h1>Coming soon.</h1>
       </div>
     );
   }
