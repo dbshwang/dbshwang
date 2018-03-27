b9e70f737f88ae849681a0e9c47f835da6e0746a
Daniel Hwang <daniel.hwang11@gmail.com>
2018-03-27 19:23:30 -0400

    Set up containers at breakpoints

--- client/src/containers/home/HomePage.js
+++ client/src/containers/home/HomePage.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HomePage extends Component {
-  componentDidMount() {
-    window.fetch('/api/drinks')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error));
-  }
-
-  render() {
-    return (
-      <div>
-        <div className='col-1 sm-col-5 md-col-8 lg-col-12 mx-auto'>
-          <h1 className='rufina gold'>Daniel Hwang</h1>
-          <h1 className='libre'>professional deep diver of the DOM</h1>
-          <h1 className='montserrat'>Coming soon</h1>
-        </div>
+const HomePage = () => {
+  return (
+    <div className='Container'>
+      <div className='col-1 sm-col-5 md-col-8 lg-col-12 mx-auto'>
+        <h1 className='rufina'>danielhwang</h1>
+        <h1 className='libre'>professional deep diver of the DOM</h1>
+        <h1 className='montserrat'>Coming soon</h1>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default HomePage;
