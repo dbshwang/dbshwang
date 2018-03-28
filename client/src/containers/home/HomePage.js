32c27112f76927a440b9753918695f2f9225232b
Daniel Hwang <daniel.hwang11@gmail.com>
2018-03-27 20:20:54 -0400

    Homepage basic implementation

--- client/src/containers/home/HomePage.js
+++ client/src/containers/home/HomePage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+const NavigationBar = () => {
+  return (
+    <div className='center'>
+      <ul className='my4 py2 mx-auto'>
+        <li className='px1-5 inline-block light caps'>Home</li>
+        <li className='px1-5 inline-block light caps'>Work</li>
+        <li className='px1-5 inline-block light caps'>Components</li>
+        <li className='px1-5 inline-block light caps'>Blog</li>
+        <li className='px1-5 inline-block light caps'>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
 const HomePage = () => {
   return (
     <div className='Container'>
-      <div className='col-1 sm-col-5 md-col-8 lg-col-12 mx-auto'>
-        <h1 className='rufina'>danielhwang</h1>
-        <h1 className='libre'>professional deep diver of the DOM</h1>
-        <h1 className='montserrat'>Coming soon</h1>
+      <NavigationBar />
+      <div className='absolute-middle center'>
+        <div className='relative'>
+          <h1 className='Name jumbo bold rufina mb0-5'>danielhwang</h1>
+          <h2 className='Barcode black libre mt1-5 absolute-middle'>professional deep diver of the DOM</h2>
+          <h2 className='h2 light montserrat'>portfolio of all things DOM related</h2>
+        </div>
       </div>
     </div>
   );
