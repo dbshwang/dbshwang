7f43619835032176ef1307d9397972e5b18b5720
Daniel Hwang <daniel.hwang11@gmail.com>
2018-03-30 17:28:47 -0400

    Set up home page animations

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const NavigationBar = () => {
+  return (
+    <div className='NavigationBar center'>
+      <ul className='my4 py2 mx-auto'>
+        <li className='px1-5 inline-block light caps'>
+          <NavLink activeClassName='active' to='/'>Home</NavLink>
+        </li>
+        <li className='px1-5 inline-block light caps'>
+          <NavLink to='/work'>Work</NavLink>
+        </li>
+        <li className='px1-5 inline-block light caps'>
+          <NavLink to='/components'>Components</NavLink>
+        </li>
+        <li className='px1-5 inline-block light caps'>
+          <NavLink to='/blog'>Blog</NavLink>
+        </li>
+        <li className='px1-5 inline-block light caps'>
+          <NavLink to='/contact'>Contact</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavigationBar;
--- client/src/containers/home/HomePage.js
+++ client/src/containers/home/HomePage.js
@@ -1,18 +1,5 @@
 import React from 'react';
-
-const NavigationBar = () => {
-  return (
-    <div className='center'>
-      <ul className='my4 py2 mx-auto'>
-        <li className='px1-5 inline-block light caps'>Home</li>
-        <li className='px1-5 inline-block light caps'>Work</li>
-        <li className='px1-5 inline-block light caps'>Components</li>
-        <li className='px1-5 inline-block light caps'>Blog</li>
-        <li className='px1-5 inline-block light caps'>Contact</li>
-      </ul>
-    </div>
-  );
-}
+import {NavigationBar} from 'components';
 
 const HomePage = () => {
   return (
@@ -20,9 +7,13 @@ const HomePage = () => {
       <NavigationBar />
       <div className='absolute-middle center'>
         <div className='relative'>
-          <h1 className='Name jumbo bold rufina mb0-5'>danielhwang</h1>
-          <h2 className='Barcode black libre mt1-5 absolute-middle'>professional deep diver of the DOM</h2>
-          <h2 className='h2 light montserrat'>portfolio of all things DOM related</h2>
+          <h1 className='Name jumbo bold rufina mb0-5'>
+            <span>danielhwang</span>
+          </h1>
+          <h2 className='Barcode mt1-5 absolute-middle jumbo libre'>
+            <span>professional deep diver of the DOM</span>
+          </h2>
+          <h2 className='Subtitle h2 light montserrat'>portfolio of all things DOM related</h2>
         </div>
       </div>
     </div>
