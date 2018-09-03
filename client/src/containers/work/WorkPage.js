dc694e38d528178ff068b901310f70545f934966
Daniel Hwang <daniel.hwang11@gmail.com>
2018-09-03 18:05:59 -0400

    Most recent to least

--- client/src/containers/work/WorkPage.js
+++ client/src/containers/work/WorkPage.js
@@ -11,17 +11,17 @@ class WorkPage extends React.Component {
   render() {
     return (
       <div className='Container'>
-        <div className='WorkSection WorkSection--Kik'>
-          <h1 className='white'>Kik.</h1>
-        </div>
-        <div className='WorkSection WorkSection--Bidvine'>
-          <h1 className='white'>Bidvine.</h1>
+        <div className='WorkSection WorkSection--Collage'>
+          <h1 className='white'>Collage</h1>
         </div>
         <div className='WorkSection WorkSection--Skywatch'>
-          <h1 className='white'>Skywatch.</h1>
+          <h1 className='white'>Skywatch</h1>
         </div>
-        <div className='WorkSection WorkSection--Collage'>
-          <h1 className='white'>Collage.</h1>
+        <div className='WorkSection WorkSection--Bidvine'>
+          <h1 className='white'>Bidvine</h1>
+        </div>
+        <div className='WorkSection WorkSection--Kik'>
+          <h1 className='white'>Kik</h1>
         </div>
       </div>
     );
